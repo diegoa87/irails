baa874ab4610ecb1b202718e1b55e9ffd602680b
Diego Arias <diagom27@gmail.com>
2014-01-18 23:04:55 -0300

    add line in production.rb config.assets.digest = true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,8 @@ Irails::Application.configure do
 
   # Code is not reloaded between requests
   config.cache_classes = true
-
+  
+  config.assets.digest = true
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
